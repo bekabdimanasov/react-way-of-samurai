deb7f32b544e6f3eeecd912a1ca6c99d008ac58a
bekabdimanasov <bekabdimanasov@gmail.com>
2020-04-12 04:42:14 +0300

    Finished lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Bek
+                </div>
+                <div className={s.dialog}>
+                    Saamai
+                </div>
+                <div className={s.dialog}>
+                    Bekjan
+                </div>
+                <div className={s.dialog}>
+                    Mama
+                </div>
+                <div className={s.dialog}>
+                    Galmurza
+                </div>
+                <div className={s.dialog}>
+                    Kylych
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hello
+                </div>
+                <div className={s.message}>
+                    How are you, Bek?
+                </div>
+                <div className={s.message}>
+                    Fine
+                </div>
+            </div>
         </div>
     )
 }
