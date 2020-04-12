366851dbdf89f6cdc33a00ba97b0ac403df7101d
bekabdimanasov <bekabdimanasov@gmail.com>
2020-04-12 08:26:50 +0300

    Finished lesson 24 UI(UserInterface) and BLL(Business Logic Layer)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,20 +20,29 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+    let dialogsData = [
+        {id:1, name:'Bek'},
+        {id:2, name:'Saamai'},
+        {id:3, name:'Bekjan'},
+        {id:4, name:'Mama'},
+        {id:5, name:'Galmurza'},
+        {id:6, name:'Kylych'}
+    ];
+
+    let messagesData = [
+        {id:1, message:'Hello'},
+        {id:2, message:'How are you, Bek?'},
+        {id:3, message:'Fine'}
+    ]
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogsItem name='Bek' id='1'/>
-                <DialogsItem name='Saamai' id='2'/>
-                <DialogsItem name='Bekjan' id='3'/>
-                <DialogsItem name='Mama' id='4'/>
-                <DialogsItem name='Galmurza' id='5'/>
-                <DialogsItem name='Kylych' id='6'/>
+                <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={s.messages}>
-                <Message message='Hello'/>
-                <Message message='How are you, Bek?'/>
-                <Message message='Fine'/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
             </div>
         </div>
     )
