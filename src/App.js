7b0576fe108af13a2a88e86b97be3cbf630666ec
bekabdimanasov <bekabdimanasov@gmail.com>
2020-04-13 11:18:18 +0300

    Finished lesson 30 - change place BrowserRouter Teg

--- src/App.js
+++ src/App.js
@@ -12,20 +12,18 @@ import {BrowserRouter, Route} from "react-router-dom"
 const App = (props) => {
 
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/profile' render={ () => <Profile state={props.state.profilePage}/> }/>
-                    <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage}/> }/>
-                    <Route path='/news' render={ () => <News/> }/>
-                    <Route path='/music' render={ () => <Music/> }/>
-                    <Route path='/settings' render={ () => <Settings/> }/>
-
-                </div>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Route path='/profile' render={() => <Profile state={props.state.profilePage}/>}/>
+                <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                <Route path='/news' render={() => <News/>}/>
+                <Route path='/music' render={() => <Music/>}/>
+                <Route path='/settings' render={() => <Settings/>}/>
             </div>
-        </BrowserRouter>);
+        </div>
+    );
 }
 
 export default App;
