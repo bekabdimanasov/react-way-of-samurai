a95b7290299095d57b39bcde0f5a6943d1264605
bekabdimanasov <bekabdimanasov@gmail.com>
2020-04-30 03:34:24 +0300

    Finished lesson 61 - cookie, login

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,13 +3,15 @@ import sidebarReducer from "./sidebar-reducer";
 import profilePageReducer from "./profile-reducer";
 import dialogsPageReducer from "./dialogs-reducer";
 import usersPageReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     /*state :    reducer*/
     profilePage: profilePageReducer,
     dialogsPage: dialogsPageReducer,
     sidebar: sidebarReducer,
-    usersPage: usersPageReducer
+    usersPage: usersPageReducer,
+    auth: authReducer
 })
 
 let store = createStore(reducers);
